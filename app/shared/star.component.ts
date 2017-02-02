77a2a9c853e79506f6105e743a576745c03ccb65
HJS <hjs@lumel.co.uk>
2017-02-02 15:28:10 +0000

    Module 7: Nested Components A nested component with input and output. Not sure the stars work thoguh...

--- app/shared/star.component.ts
+++ app/shared/star.component.ts
@@ -1,29 +1,26 @@
 /**
  * Created by hjs on 02/02/2017.
  */
-import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnChanges, Input,
+         Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'ai-star',
     moduleId: module.id,
     templateUrl: 'star.component.html'
 })
-export class StarComponent
-    implements OnChanges
-{
+export class StarComponent implements OnChanges {
     @Input() rating: number;
     starWidth: number;
 
-    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
+    @Output() ratingClicked: EventEmitter<string> =
+        new EventEmitter<string>();
 
-     ngOnChanges(): void {
-         this.starWidth = this.rating * 86/5;
+    ngOnChanges(): void {
+        this.starWidth = this.rating * 86 / 5;
     }
 
-    onClick()
-    {
+    onClick(): void {
         this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
     }
-
-
 }
